a94a950c994a04f1f77b237efe425d93feeb67ac
Your Name <jomerubaldo@gmail.com>
2025-08-12 23:39:57 +0800

    initial commit

--- function.js
+++ function.js
@@ -1,17 +1,33 @@
-const countValue = document.querySelector('#count');
-const addButton = document.querySelector('#add');
-const minusButton = document.querySelector('#minus');
+// Get reference
+const input = document.querySelector('#inputText');
+const button = document.querySelector('#addText');
+const list = document.querySelector('#listText');
 
-let count = 0;
+// Function
+button.addEventListener('click', myFunction);
+function myFunction() {
+  // Create li list
+  const node = document.createElement('li');
 
-addButton.addEventListener('click', myAddFunction);
-function myAddFunction() {
-  count++;
-  countValue.innerText = count;
-}
+  // Get user input value
+  const getValue = input.value;
 
-minusButton.addEventListener('click', myMinusFunction);
-function myMinusFunction() {
-  count--;
-  countValue.innerText = count;
+  // Do the function if user have value else show alert message
+  if (getValue) {
+    // Get user value and createTextNode
+    const getuserinput = document.createTextNode(getValue);
+
+    // Get uservalue with li and display using appendChild
+    node.appendChild(getuserinput);
+
+    // Get main list id and display the final value
+    document.querySelector('#listText').appendChild(node);
+
+    // clear input text every add text
+    input.value = '';
+
+    // click without value show alert message
+  } else {
+    alert('Please Enter Text');
+  }
 }
